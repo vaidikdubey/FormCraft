879b0b4b1635c3844fd0f0ca9ff25394ff2c7271
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-01-17 15:23:57 +0530

    Added payment routes and controllers for razorpay test payment integration

---
+++ backend/src/controllers/payment.controllers.js
@@ -0,0 +1,67 @@
+import { Razorpay } from "razorpay";
+import crypty from "crypto";
+import { User } from "../models/user.model.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { ApiError } from "../utils/api-error.js";
+import { UserRolesEnum } from "../utils/constants.js";
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+const createOrder = asyncHandler(async (req, res) => {
+  const amount = 499; //INR 49900 paise
+
+  const options = {
+    amount: amount * 100, //convert amount to paise
+    currency: "INR",
+    receipt: `receipt_${req.user.id}_${Date.now()}`,
+    payment_capture: 1,
+  };
+
+  const order = await razorpay.orders.create(options);
+
+  res.status(200).json(
+    new ApiResponse(200, {
+      orderId: order.id,
+      amount: order.amount,
+      currency: order.currency,
+      key: process.env.RAZORPAY_KEY_ID,
+    }),
+  );
+});
+
+const verifyPayment = asyncHandler(async (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+
+  if (!razorpay_order_id || !razorpay_payment_id || !razorpay_signature)
+    throw new ApiError(400, "Payment details missing");
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypty
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .update(body)
+    .digest("hex");
+
+  if (expectedSignature !== razorpay_signature)
+    throw new ApiError(400, "Invalid payment signature");
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) throw new ApiError(404, "User not found");
+
+  user.role = UserRolesEnum.PAID;
+  user.responseLimit = 250;
+
+  await user.save();
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, user, "Payment verified. PRO plan activated!"));
+});
+
+export { createOrder, verifyPayment };
--- backend/src/routes/payment.routes.js
+++ backend/src/routes/payment.routes.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import { isLoggedIn } from "../middlewares/auth.middleware.js";
+import {
+  createOrder,
+  verifyPayment,
+} from "../controllers/payment.controllers.js";
 
 const router = Router();
+
+router.route("/create").post(isLoggedIn, createOrder);
+
+router.route("/verify").post(isLoggedIn, verifyPayment);
 
 export default router;
